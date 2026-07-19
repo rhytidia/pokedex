6716a9db4b79bde954e6e5a2480fd00e2bd00a8c
rhytidia <264509465+rhytidia@users.noreply.github.com>
2026-07-19 15:44:11 -0700

    starter code for pokedex REPL; also fixed tsconfig to deal with type issue

--- src/repl.ts
+++ src/repl.ts
@@ -1,6 +1,31 @@
+import { createInterface } from "readline";
+
+export function startREPL(): void {
+	const rl = createInterface ({
+		input: process.stdin,
+		output: process.stdout,
+		prompt: "Pokedex > "
+	})
+
+	rl.prompt()
+	rl.on('line', (input) => {
+		const cleaned = cleanInput(input);
+		if (cleaned.length === 0) {
+			rl.prompt();
+			return;
+		} else {
+			console.log(`Your command was: ${cleaned[0]}`);
+			rl.prompt();
+		}
+	});
+}
+
 export function cleanInput(input: string): string[] {
-    const lowerCase: string = input.toLowerCase();
-	return lowerCase.trim().split(/\s+/).filter((word) => word !== "");
+	const lowerCase: string = input.toLowerCase();
+	return lowerCase
+		.trim()
+		.split(/\s+/)
+		.filter((word) => word !== "");
 }
 
 // string.split(/\s+/) will split based on any number of whitespaces using regex,
